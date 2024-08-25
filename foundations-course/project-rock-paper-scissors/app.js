03d39e199d0fba8080e6626848ebc858d8918790
Jeevan Sadalge <jeevanhrx@gmail.com>
2024-08-25 18:51:17 +0530

    rock paper scissors project complete

---
+++ foundations-course/project-rock-paper-scissors/app.js
@@ -0,0 +1,29 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function gerComputerChoice() {
+    let arr = ['rock', 'paper', 'scissors'];
+    let ind = Math.floor(Math.random() * 3);
+    return arr[ind];
+}
+
+function getHumanChoice() {
+    return prompt()
+}
+
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    if (humanChoice === computerChoice) humanScore++
+    else computerScore++;
+}
+
+function playGame() {
+    for (let i = 1; i <= 5; i++) {
+        let humanChoice = getHumanChoice();
+        let computerChoice = gerComputerChoice();
+        playRound(humanChoice, computerChoice);
+    }
+    console.log(`Score is : Human = ${humanScore} and Computer = ${computerScore}. ${humanScore >= computerScore ? 'You' : 'Computer'} won the game.`);
+}
+
+playGame()
